cf58ef4ca90023e2e7c53e74db1760099ad204ae
roopei <roope.ikonen@phz.fi>
2018-08-22 13:38:30 +0300

    Toinen endpointti lisatty

--- initial/src/main/java/hello/ApplicationController.java
+++ initial/src/main/java/hello/ApplicationController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,12 @@ public class ApplicationController {
     @RequestMapping("/")
     public String index() {
         return "Hello, hello\n";
+    }
+
+    @RequestMapping("/{name}")
+    public String sayHello(@PathVariable String name) {
+
+        return "Hello " + name + "\n";
     }
 
 }
